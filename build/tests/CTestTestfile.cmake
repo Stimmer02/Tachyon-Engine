aa2ff8d1712129153700b0c21e3f07beb5865e82
DavidSolz <keralis@o2.pl>
2023-10-13 19:40:29 +0200

    TE-19 CI/CD rewrite

--- build/tests/CTestTestfile.cmake
+++ build/tests/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test([=[TachyonEngine_test]=] "C:/Users/Dawid/Desktop/ProjektZespolowy/tachion-engine/build/tests/TachyonEngine_test.exe")
-set_tests_properties([=[TachyonEngine_test]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Dawid/Desktop/ProjektZespolowy/tachion-engine/tests/CMakeLists.txt;34;add_test;C:/Users/Dawid/Desktop/ProjektZespolowy/tachion-engine/tests/CMakeLists.txt;0;")
+set_tests_properties([=[TachyonEngine_test]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Dawid/Desktop/ProjektZespolowy/tachion-engine/tests/CMakeLists.txt;11;add_test;C:/Users/Dawid/Desktop/ProjektZespolowy/tachion-engine/tests/CMakeLists.txt;0;")
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 
-#define CL_HPP_TARGET_OPENCL_VERSION 200
+#define CL_TARGET_OPENCL_VERSION 200
 
 #include <CL/cl.h>
 #include <GLFW/glfw3.h>
