3fae98a105bbbdd4aab8c0ab55174e6c6804aa69
emil.szerafn <mojzesz2002@gmail.com>
2023-11-09 22:07:36 +0100

    deletion of second main

--- src/PhysicsProcessor/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "PhysicsProcessorBuilder.h"
-
-
-int main(){
-    PhysicsProcessorBuilder PPB;
-    engineConfig config;
-
-    PPB.build("./engine_kernel_fragments/", "./engine_structs/", 1, config, 0, 0);
-}
