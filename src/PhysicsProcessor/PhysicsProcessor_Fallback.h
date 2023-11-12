7e388fb286ffbb55b47cd53d5422de8714154c09
Rafał Szulc <r.s.szulc@gmail.com>
2023-11-12 00:32:18 +0000

    Upload New File

---
+++ src/PhysicsProcessor/PhysicsProcessor_Fallback.h
@@ -0,0 +1,27 @@
+#ifndef _PHYSICSPROCESSOR_FALLBACK_H
+#define _PHYSICSPROCESSOR_FALLBACK_H
+
+#include "IPhysicsProcessor.h"
+
+
+class PhysicsProcessor_Fallback : public IPhysicsProcessor{
+public:
+    PhysicsProcessor_Fallback(cl::Context openCLContext, cl::Kernel engine, GLuint PBO, engineConfig config, cl::Device device);
+    ~PhysicsProcessor_Fallback() override;
+    void generateFrame() override;
+    void spawnVoxel(uint x, uint y, uint substanceID) override;
+    uint countVoxels() override;
+
+private:
+    engineConfig config;
+    //resources: need to think about it
+    cl::Context context;
+    cl::Kernel engine;
+    cl::CommandQueue queue;
+    cl::Device device;
+    cl_mem pbo_mem;
+    struct voxel* voxels;
+    uint size;
+};
+
+#endif
