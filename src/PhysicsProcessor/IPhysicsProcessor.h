a224edf18191a799151cf5e4642a26ba66953f23
Rafał Szulc <r.s.szulc@gmail.com>
2023-11-16 20:10:16 +0000

    Update IPhysicsProcessor.h -stupidly I added engine config to this file, deleting this

--- src/PhysicsProcessor/IPhysicsProcessor.h
+++ src/PhysicsProcessor/IPhysicsProcessor.h
@@ -80,19 +80,3 @@ public:
 };
 
 #endif
-
-#ifndef _ENGINECONFIG_H
-#define _ENGINECONFIG_H
-
-#include <CL/opencl.hpp>
-typedef unsigned int uint;
-
-struct engineConfig{
-    uint simulationWidth;
-    uint simulationHeight;
-    float gravity;
-    float timefactor;
-    float atmosphereViscosity;
-};
-
-#endif
