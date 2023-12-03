16898be720b96d3655f6517e1fd606a50ddaae95
Rafał Szulc <r.s.szulc@gmail.com>
2023-12-03 10:56:17 +0000

    Update PhysicsProcessor.h - beta version fix - arguments of constructorMain and deleting useless struct

--- src/PhysicsProcessor/PhysicsProcessor.h
+++ src/PhysicsProcessor/PhysicsProcessor.h
@@ -13,7 +13,7 @@ public:
     void allocateHostMemory(cl::Context openCLContext, cl::Kernel engine, GLuint PBO, engineConfig config, cl::Device device);
     std::string structuresAsString();
     std::string kernelCodeAsString();
-	void constructorMain(cl::Context openCLContext, struct engineConfig config, cl::Device device);
+	void constructorMain(cl::Context openCLContext, engineConfig config, cl::Device device);
 	void configureMainKernel();
     PhysicsProcessor(cl::Context openCLContext, cl::Kernel engine, GLuint PBO, engineConfig config, cl::Device device);
     ~PhysicsProcessor();
