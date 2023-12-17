037343d3cc95c0b3ab81c29a8f8d892563f4547c
DavidSolz <keralis@o2.pl>
2023-12-17 22:40:29 +0100

    BitmapReader implementation and Image structure

---
+++ src/GUIRenderer/Image.h
@@ -0,0 +1,17 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <stdint.h>
+#include "Color.h"
+
+struct Image {
+    uint32_t width;
+    uint32_t height;
+    uint32_t image_size;
+    int32_t x_pixels_per_m;
+    int32_t y_pixels_per_m;
+    Color * pixels;
+};
+
+
+#endif
