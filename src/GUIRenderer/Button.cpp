461ff7813789932ddf626f563a3ad30356762079
DavidSolz <keralis@o2.pl>
2024-01-28 19:19:31 +0100

    Red triangle

--- src/GUIRenderer/Button.cpp
+++ src/GUIRenderer/Button.cpp
@@ -9,11 +9,19 @@ Button::Button(const int& x, const int& y, const int& width, const int& height) 
 		0.0f,  0.5f, 0.0f
 	};
 
-	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 }
 
 void Button::Render(){
@@ -21,11 +29,11 @@ void Button::Render(){
 	if(sprite != nullptr)
 		sprite->Load();
 
-        // glBindVertexArray(VAO);
+        glBindVertexArray(VAO);
 
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        // glBindVertexArray(0);
+        glBindVertexArray(0);
 
 	if(sprite != nullptr)
 		sprite->UnLoad();
