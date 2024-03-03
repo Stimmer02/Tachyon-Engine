bb9a7eab88277a5d63fc100dc42b9cb5180de411
Rafał Szulc <r.s.szulc@gmail.com>
2024-03-03 19:53:26 +0000

    Taking Component class and class Node for nodes in our tree.

--- src/GUIRenderer/RedBlackTree.cpp
+++ src/GUIRenderer/RedBlackTree.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include "Component.h"
+using namespace std;
 
+struct Node {
+    Component component;
+
+    Node* parent;
+    Node* left;
+    Node* right;
+    
+    int color;
+};
