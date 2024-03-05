58ed42accdeaadcf78857fc8a231ebad34d7ce28
Rafał Szulc <r.s.szulc@gmail.com>
2024-03-05 15:46:42 +0000

    Added rotation functions.

--- src/GUIRenderer/RedBlackTree.cpp
+++ src/GUIRenderer/RedBlackTree.cpp
@@ -30,4 +30,63 @@ public:
         // We want to have an empty root, but no irritating errors.
         root = nodeNull;
     }
+
+    void rotationLeft(Node* center) {
+        Node* rightNode = center->right;
+
+        // Left son of right transfered to center's right son.
+        center->right = rightNode->left;
+        if (rightNode->left != nodeNull) {
+            rightNode->left->parent = center;
+        }
+
+        // Making right node into new center.
+        rightNode->parent = center->parent;
+        // If center is root.
+        if (center->parent == nullptr) {
+            this->root = rightNode;
+        }
+        // If center is left son.
+        else if (center == center->parent->left) {
+            center->parent->left = rightNode;
+        }
+        // If center is right son.
+        else {
+            center->parent->right = rightNode;
+        }
+
+        // Making left node into new center.
+        rightNode->left = center;
+        center->parent = rightNode;
+    }
+
+    // The same as left rotate, but everything other way around.
+    void rotationRight(Node* center) {
+        Node* leftNode = center->left;
+
+        // Right son of left transfered to center's left son.
+        center->left = leftNode->right;
+        if (leftNode->right != nodeNull) {
+            leftNode->right->parent = center;
+        }
+
+        // Making left node into new center.
+        leftNode->parent = center->parent;
+        // If center is root.
+        if (center->parent == nullptr) {
+            this->root = leftNode;
+        }
+        // If center is left son.
+        else if (center == center->parent->left) {
+            center->parent->left = leftNode;
+        }
+        // If center is right son.
+        else {
+            center->parent->right = leftNode;
+        }
+
+        // Making right node into new center.
+        leftNode->right = center;
+        center->parent = leftNode;
+    }
 }
