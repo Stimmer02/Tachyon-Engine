9127b575df5145ddfba3fac067b80ac99029cd89
Rafał Szulc <r.s.szulc@gmail.com>
2024-03-06 09:48:18 +0000

    Add new file - interface of RedBlackTree

---
+++ src/GUIRenderer/RedBlackTree.h
@@ -0,0 +1,50 @@
+#ifndef REDBLACKTREE_H
+#define REDBLACKTREE_H
+
+#include <string.h>
+using namespace std;
+
+struct Node {
+    Component* component;
+
+    Node* parent;
+    Node* left;
+    Node* right;
+
+    int color;
+};
+
+struct Node {
+    Component* component;
+
+    Node* parent;
+    Node* left;
+    Node* right;
+
+    int color;
+};
+
+class RedBlackTree {
+protected:
+    // Root.
+    Node* root;
+    // Sneaky way of having node-like None.
+    Node* nodeNull;
+
+    Node* query_rec(Component* comp, Node* suspect);
+
+    void harderPaintingProblems(Node* repaintingNode);
+
+public:
+    RedBlackTree();
+
+    Node* query(Component* comp);
+
+    void rotationLeft(Node* center);
+
+    void rotationRight(Node* center);
+
+    void add(Component* comp);
+};
+
+#endif
