438859746e6370ba987b5234658ef5fb0f729b75
Rafał Szulc <r.s.szulc@gmail.com>
2024-03-20 18:05:55 +0000

    added SharedNameResolver.h

---
+++ src/ECSystem/SharedNameResolver.h
@@ -0,0 +1,28 @@
+#ifndef REDBLACKTREE_H
+#define REDBLACKTREE_H
+
+#include <string.h>
+using namespace std;
+
+const int ALPHABET_SIZE = 26;
+
+struct Node {
+    Node* next_char[ALPHABET_SIZE];
+    const int32_t size = 0;
+    const void * pointer;
+};
+
+class SharedNameResolver {
+protected:
+    // Root.
+    Node* root;
+
+public:
+    SharedNameResolver();
+
+    void Emplace(const std::string & _name, const void * _pointer, const int32_t & _size = 1);
+    void * Find(const std::string & _name, const int32_t & _elem_size = 0, const int32_t & _element = 0);
+    void Resize(const std::string & _name, const int32_t & _size);
+};
+
+#endif
