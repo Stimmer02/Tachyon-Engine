34dec5d8345ba2231d50b7f14a263efd01f18a71
emil.szerafin <mojzesz2002@gmail.com>
2024-05-12 09:09:20 +0200

    Sprzątanie i poprawa nieczytelnego kodu

--- src/PhysicsProcessor/StructTree.cpp
+++ src/PhysicsProcessor/StructTree.cpp
@@ -1,19 +1,14 @@
-#include <iostream>
-#include <queue>
 #include "StructTree.h"
-#include <sys/stat.h>
-#include<fstream>
-#include<sstream>
-#include "ClStructParser.h"
-#include <set>
 
 using namespace std;
 
 StructTree::StructTree() {
-
+    this->status = -1;
+    this->root = nullptr;
 }
 
 StructTree::StructTree(std::string structDirectory, std::string rootStructName) {
+    this->status = -1;
     setStructDirectory(structDirectory);
     setRootStruct(rootStructName);
 }
@@ -44,7 +39,7 @@ char StructTree::setRootStruct(std::string rootStructName) {
 
     MacroManager macroManager;
     SizeCalculator sCalc(8);
-    ClStructParser clParser(&macroManager, &sCalc);
+    ClStructParser clParser(&macroManager);
 
     if (macroManager.parseFile("macro/macros.cfg")){
         std::fprintf(stderr, "Error has occured\n");
@@ -65,13 +60,20 @@ char StructTree::build(ClStructParser* parser) {
     q.push(root);
 
     while (q.empty() == false) {
-        engineStruct* firstStruct = q.front();
+        engineStruct* currentStructure = q.front();
         q.pop();
 
-        for (int i = 0; i < firstStruct->fieldCount; ++i) {
-            if (firstStruct->fields[i].type == engineStruct::cl_struct) {
-                string path = this->structDirectory + "/" + firstStruct->fields[i].subStructName + ".cl";
-                string path2 = this->structDirectory + "/" + firstStruct->fields[i].subStructName + ".clcpp";
+        for (int i = 0; i < currentStructure->fieldCount; ++i) {
+            if (currentStructure->fields[i].type == engineStruct::cl_struct) {
+
+                if (parsedStructs.find(currentStructure->fields[i].subStructName) != parsedStructs.end()) {
+                    currentStructure->fields[i].subStruct = parsedStructs[currentStructure->fields[i].subStructName].node;
+                    // q.push(currentStructure->fields[i].subStruct); not sure if this is needed
+                    continue;
+                }
+
+                string path = this->structDirectory + "/" + currentStructure->fields[i].subStructName + ".cl";
+                string path2 = this->structDirectory + "/" + currentStructure->fields[i].subStructName + ".clcpp";
 
                 const char* file = path.c_str();
                 const char* file2 = path2.c_str();
@@ -82,38 +84,26 @@ char StructTree::build(ClStructParser* parser) {
                     ostringstream s;
                     s << f.rdbuf();
                     code = s.str();
-                }
-
-                ifstream f2(path2);
-                string code2;
-                if(f2) {
-                    ostringstream s;
-                    s << f2.rdbuf();
-                    code2 = s.str();
-                }
-
-                struct stat sb;
-                if (stat(file, &sb) == 0 and !(sb.st_mode & S_IFDIR)) {
-                    firstStruct->fields[i].subStruct = parser->processStruct(code);
-                    if (firstStruct->fields[i].subStruct == nullptr) {
-                        status = 1;
+                } else {
+                    f.close();
+                    f.open(path2);
+                    if (f) {
+                        ostringstream s;
+                        s << f.rdbuf();
+                        code = s.str();
+                    } else {
+                        this->status = 1;
                         return status;
                     }
-                    q.push(firstStruct->fields[i].subStruct);
                 }
-                else if (stat(file2, &sb) == 0 and !(sb.st_mode & S_IFDIR)) {
-                    firstStruct->fields[i].subStruct = parser->processStruct(code2);
-                    if (firstStruct->fields[i].subStruct == nullptr) {
-                        status = 1;
-                        return status;
-                    }
-                    q.push(firstStruct->fields[i].subStruct);
-                }
-                else{
-                    this->status = 1;
+
+                currentStructure->fields[i].subStruct = parser->processStruct(code);
+                parsedStructs[currentStructure->fields[i].subStructName].node = currentStructure->fields[i].subStruct;
+                if (currentStructure->fields[i].subStruct == nullptr) {
+                    status = 1;
                     return status;
                 }
-
+                q.push(currentStructure->fields[i].subStruct);
             }
         }
     }
@@ -122,27 +112,100 @@ char StructTree::build(ClStructParser* parser) {
     return status;
 }
 
-std::string StructTree::getError() {
-    return this->error;
-}
+char StructTree::calculateSizes(SizeCalculator* sCalc) {
+    if (status != 0) {
+        return status;
+    }
 
-set<string> used;
+    queue<engineStruct*> q;
+    q.push(root);
 
-std::string StructTree::getStructuresHelper(engineStruct* node) {
-    std::string result = "";
-    for (int i = 0; i < node->fieldCount; ++i) {
-        if (node->fields[i].subStruct != nullptr) {
-            result += getStructuresHelper(node->fields[i].subStruct);
+    while (q.empty() == false) {
+        engineStruct* currentStructure = q.front();
+        q.pop();
+
+        for (int i = 0; i < currentStructure->fieldCount; ++i) {
+            if (currentStructure->fields[i].type == engineStruct::cl_struct) {
+                q.push(currentStructure->fields[i].subStruct);
+            }
+        }
+
+        if (sCalc->calculate(currentStructure) != 0) {
+            this->status = 2;
+            return status;
         }
     }
 
-    if (used.find(node->name) == used.end()) {
+    this->status = 0;
+    return status;
+}
+
+void StructTree::printTree() {
+    if (status != 0) {
+        return;
+    }
+    setVisitedFalse();
+    printTreeRecursive(root);
+}
+
+void StructTree::printTreeRecursive(engineStruct* node) {
+    for (int i = 0; i < node->fieldCount; ++i) {
+        if (node->fields[i].subStruct != nullptr) {
+            printTreeRecursive(node->fields[i].subStruct);
+        }
+    }
+
+    parsedStruct& parsed = parsedStructs[node->name];
+
+    if (parsed.visited == false) {
+        engineStruct* structure = parsed.node;
+        std::printf("name: %s; size: %d; field count: %d\n", structure->name.c_str(), structure->byteSize, structure->fieldCount);
+        for (uint i = 0; i < structure->fieldCount; i++){
+            if (structure->fields[i].type == engineStruct::cl_struct){
+                std::printf(" %d) var: %s; type: %d; size: %d; struct: %s; amount: %d\n", i, structure->fields[i].name.c_str(), structure->fields[i].type, structure->fields[i].byteSize, structure->fields[i].subStructName.c_str(), structure->fields[i].arrSize);
+            } else if (structure->fields[i].arrSize > 0){
+                std::printf(" %d) var: %s; type: %d; size: %d; pointer: %d\n", i, structure->fields[i].name.c_str(), structure->fields[i].type, structure->fields[i].byteSize, structure->fields[i].arrSize);
+            } else {
+                std::printf(" %d) var: %s; type: %d; size: %d\n", i, structure->fields[i].name.c_str(), structure->fields[i].type, structure->fields[i].byteSize);
+            }
+        }
+        parsed.visited = false;
+    }
+}
+
+std::string StructTree::getStructuresRecursive(engineStruct* node) {
+    std::string result = "";
+    for (int i = 0; i < node->fieldCount; ++i) {
+        if (node->fields[i].subStruct != nullptr) {
+            result += getStructuresRecursive(node->fields[i].subStruct);
+        }
+    }
+
+    parsedStruct& parsed = parsedStructs[node->name];
+
+    if (parsed.visited == false) {
         result += node->rawCode;
-        used.insert(node->name);
+        parsed.visited = false;
     }
     return result;
 }
 
-std::string StructTree::getStructures() {
-    return getStructuresHelper(root);
+std::string StructTree::getError() {
+    return this->error;
 }
+
+std::string StructTree::getStructures() {
+    if (status != 0) {
+        return "";
+    }
+    setVisitedFalse();
+    return getStructuresRecursive(root);
+}
+
+void StructTree::setVisitedFalse() {
+    for (auto& it : parsedStructs) {
+        it.second.visited = false;
+    }
+}
+
+
--- src/PhysicsProcessor/StructTree.h
+++ src/PhysicsProcessor/StructTree.h
@@ -3,6 +3,15 @@
 
 #include "engineStruct.h"
 #include "ClStructParser.h"
+#include "SizeCalculator.h"
+
+#include <map>
+#include<fstream>
+#include<sstream>
+#include <iostream>
+#include <queue>
+#include <sys/stat.h>
+
 
 class StructTree{
     public:
@@ -13,14 +22,26 @@ class StructTree{
         char setRootStruct(std::string rootStructName); //Parses and saves the root struct
         void setStructDirectory(std::string structDirectory); //Sets the root directory where the structs are stored (they could be stored recursively in subdirectories)
         char build(ClStructParser* parser); //Builds the tree of structures starting from the root
+        char calculateSizes(SizeCalculator* sCalc); //Calculates the sizes of the structures in the tree
+        void printTree(); //Prints the structures in the tree
         std::string getError(); //Returns the error message
-        std::string getStructuresHelper(engineStruct* node);
         std::string getStructures(); //Returns structures code in a single string in the order from leaves to the root (format that is ready to be compiled)
 
     private:
+        std::string getStructuresRecursive(engineStruct* node); //Recursive function that returns structures code in a single string in the order from leaves to the root
+        void setVisitedFalse(); //goes through parsedStructs and sets visited to false
+        void printTreeRecursive(engineStruct* node); //Recursive function that prints the structures in the tree
+
         engineStruct* root; //Parsed root struct
         std::string structDirectory; //Root directory where the structs are stored
         std::string error; //Error message
+
+        struct parsedStruct{
+            engineStruct* node;
+            bool visited;
+        };
+
+        std::map<std::string, parsedStruct> parsedStructs; //Map of parsed structs
         char status; //Status of the tree building process:  < -1 not ready to be builded, -1 - ready to be builded, 0 - builded, > 0 - build error code
 };
 #endif
